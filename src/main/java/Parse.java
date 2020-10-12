bcac217371cd197cd737542fb350db6bc52492e3
Armaan Singh <armaan.singh@gmail.com>
2020-10-12 12:43:23 -0700

    Finished Project

--- src/main/java/Parse.java
+++ src/main/java/Parse.java
@@ -1,6 +1,22 @@
 import java.util.*;
 
 public class Parse {
+    private static final Map<String, Integer> TOKEN_MAP = new HashMap<String, Integer>();
+    static{
+        TOKEN_MAP.put("{", 0);
+        TOKEN_MAP.put("}", 1);
+        TOKEN_MAP.put("System.out.println", 2);
+        TOKEN_MAP.put("(", 3);
+        TOKEN_MAP.put(")", 4);
+        TOKEN_MAP.put(";", 5);
+        TOKEN_MAP.put("if", 6);
+        TOKEN_MAP.put("else", 7);
+        TOKEN_MAP.put("while", 8);
+        TOKEN_MAP.put("true", 9);
+        TOKEN_MAP.put("false", 10);
+        TOKEN_MAP.put("!", 11);
+    };
+
     private static final List<String> TERMINALS = new ArrayList<>(
             Arrays.asList(
                     "{",
@@ -18,14 +34,19 @@ public class Parse {
             )
     );
 
+    private static final int BR_OPEN = 0, BR_CLO = 1, PRINT = 2, P_OPEN = 3, P_CLO = 4,
+            SEMI = 5, IF = 6, ELSE = 7, WHILE = 8, TRUE = 9, FALSE = 10, BANG = 11, EOF = 12;
+
+    private int tok;
+    private int err_flag = 0;
+
     public static void main(String[] args) {
         // Setup scanner and tokens list
         Scanner sc = new Scanner(System.in);
-        List<String> tokens = new ArrayList();
+        List<String> tokenStrings = new ArrayList();
+        ArrayList<Integer> tokens = new ArrayList<>();
         String program = "";
 
-        // SCANNER
-        // tokenize entire program
         while (sc.hasNextLine()) {
             String s = sc.nextLine();
             program = program + s + "\n";
@@ -38,13 +59,30 @@ public class Parse {
                         return;
                     }
                 } else {
-                    tokens.add(next);
+                    tokenStrings.add(next);
                 }
             }
         }
 
+        for (String token : tokenStrings) {
+            tokens.add(TOKEN_MAP.get(token));
+        }
+        tokens.add(EOF);
+
+        //System.out.println(tokenStrings);
+        //System.out.println(tokens);
+
         // PARSER
         // Parse tokens
+        Parser parser = new Parser(tokens);
+        int out = parser.parse();
 
+        if (out == 0) {
+            System.out.println("Program parsed successfully");
+        } else {
+            System.out.println("Parse error");
+        }
     }
+
+
 }
---
+++ src/main/java/Parser.java
@@ -0,0 +1,156 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Parser {
+    private static final int BR_OPEN = 0, BR_CLO = 1, PRINT = 2, P_OPEN = 3, P_CLO = 4,
+            SEMI = 5, IF = 6, ELSE = 7, WHILE = 8, TRUE = 9, FALSE = 10, BANG = 11, EOF = 12;
+
+    public Parser(ArrayList<Integer> tokens){
+        this.tokens = tokens;
+    }
+
+    public int parse() {
+        tok = getToken();
+        S();
+
+        if (!tokens.isEmpty()) err_flag = 1;
+
+        return err_flag;
+    }
+
+    private int err_flag = 0;
+    private int tok;
+    private List<Integer> tokens;
+
+    private void error(){
+        err_flag = 1;
+    }
+
+    private int getToken() {
+        return tokens.remove(0);
+    }
+
+    private void advance() {
+        tok = getToken();
+    }
+
+    private void eat(int t) {
+        if (tok == t) {
+            advance();
+        } else {
+            error();
+        }
+    }
+
+    private void S() {
+        switch (tok) {
+            case BR_OPEN:
+                eat(BR_OPEN);
+                L();
+                T_prime();
+                break;
+            case PRINT:
+                eat(PRINT);
+                eat(P_OPEN);
+                E();
+                F_prime();
+                Z_prime();
+                break;
+            case IF:
+                eat(IF);
+                eat(P_OPEN);
+                E();
+                G_prime();
+                break;
+            case WHILE:
+                eat(WHILE);
+                eat(P_OPEN);
+                E();
+                X_prime();
+                break;
+            default:
+                error();
+        }
+    }
+
+    private void L() {
+        switch (tok) {
+            case BR_OPEN:
+            case WHILE:
+            case IF:
+            case PRINT:
+                S();
+                L();
+                break;
+        }
+    }
+
+    private void E() {
+        switch (tok) {
+            case TRUE:
+                eat(TRUE);
+                break;
+            case FALSE:
+                eat(FALSE);
+                break;
+            case BANG:
+                eat(BANG);
+                E();
+                break;
+            default:
+                error();
+        }
+    }
+
+    private void B_prime() {
+        switch (tok) {
+            case ELSE:
+                eat(ELSE);
+                S();
+                break;
+            default:
+                error();
+        }
+    }
+
+    private void G_prime() {
+        F_prime();
+        S();
+        B_prime();
+    }
+
+    private void T_prime() {
+        switch (tok) {
+            case BR_CLO:
+                eat(BR_CLO);
+                break;
+            default:
+                error();
+        }
+    }
+
+    private void F_prime() {
+        switch (tok) {
+            case P_CLO:
+                eat(P_CLO);
+                break;
+            default:
+                error();
+        }
+    }
+
+    private void Z_prime() {
+        switch (tok) {
+            case SEMI:
+                eat(SEMI);
+                break;
+            default:
+                error();
+        }
+    }
+
+    private void X_prime() {
+        F_prime();
+        S();
+    }
+}
