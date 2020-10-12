ef8c60af060b5bfd1afefb5438e3f6c82d6d46d0
Armaan Singh <armaan.singh@gmail.com>
2020-10-11 17:07:50 -0700

    Implemented Scanner

--- src/main/java/Parse.java
+++ src/main/java/Parse.java
@@ -1,7 +1,50 @@
+import java.util.*;
+
 public class Parse {
-    public static void main(String [] args) {
-        /* This is a placeholder file,
-            please create a new class with required name for the homework
-            and remove this file */
+    private static final List<String> TERMINALS = new ArrayList<>(
+            Arrays.asList(
+                    "{",
+                    "}",
+                    "System.out.println",
+                    "(",
+                    ")",
+                    ";",
+                    "if",
+                    "else",
+                    "while",
+                    "true",
+                    "false",
+                    "!"
+            )
+    );
+
+    public static void main(String[] args) {
+        // Setup scanner and tokens list
+        Scanner sc = new Scanner(System.in);
+        List<String> tokens = new ArrayList();
+        String program = "";
+
+        // SCANNER
+        // tokenize entire program
+        while (sc.hasNextLine()) {
+            String s = sc.nextLine();
+            program = program + s + "\n";
+            StringTokenizer st = new StringTokenizer(s, " !(){}", true);
+            while (st.hasMoreTokens()) {
+                String next = st.nextToken();
+                if (!TERMINALS.contains(next)){
+                    if (!next.trim().isEmpty()) {
+                        System.out.println("Parse error");
+                        return;
+                    }
+                } else {
+                    tokens.add(next);
+                }
+            }
+        }
+
+        // PARSER
+        // Parse tokens
+
     }
 }
